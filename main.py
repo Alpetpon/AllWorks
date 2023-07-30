84b6d12fa1e459732e2e4dae708449973ffcdde5
Alpetpon <ALpetpon51pro@gmail.com>
2023-07-30 18:27:35 +0300

    simple main + handlers

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import asyncio
+import logging
+from aiogram.types import Message
+from aiogram import Bot, Dispatcher
+from aiogram.filters.command import Command
+from config import BOT_TOKEN
+from handlers import router
 
+
+@router.message(Command(commands=["start"]))
+async def command_start_handler(message: Message) -> None:
+    await message.answer(f"Приветствую, <b>{message.from_user.full_name}!</b> Я помогу тебя в поиске работы мечты")
+
+
+
+
+async def main() -> None:
+    dp = Dispatcher()
+    dp.include_router(router)
+    bot = Bot(BOT_TOKEN, parse_mode="HTML")
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
