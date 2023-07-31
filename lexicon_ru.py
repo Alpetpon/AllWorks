bcb6f53b96de0d98b41ebadcabbe3babf42cec90
Alpetpon <ALpetpon51pro@gmail.com>
2023-07-31 16:48:35 +0300

    Start develop - create menu

---
+++ lexicon_ru.py
@@ -0,0 +1,5 @@
+LEXICON_COMMANDS_RU: dict[str, str] = {
+                '/command_1': 'command_1 desription',
+                '/command_2': 'command_2 desription',
+                '/command_3': 'command_3 desription',
+                '/command_4': 'command_4 desription'}
--- main.py
+++ main.py
@@ -3,11 +3,13 @@ import logging
 from aiogram import Bot, Dispatcher
 from config import BOT_TOKEN
 from handlers import router
+from set_menu import set_main_menu
 
 async def main() -> None:
     dp = Dispatcher()
     dp.include_router(router)
     bot = Bot(BOT_TOKEN, parse_mode="HTML")
+    await set_main_menu(bot)
     await dp.start_polling(bot)
 
 
