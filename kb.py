eb37944263c306bb7346db5cb673b0a0c2ad349a
Alpetpon <ALpetpon51pro@gmail.com>
2023-07-31 16:00:08 +0300

    Start develop - start and choise(first level)button

--- kb.py
+++ kb.py
@@ -7,8 +7,5 @@ button_1: KeyboardButton = KeyboardButton(text=text.prof_true)
 button_2: KeyboardButton = KeyboardButton(text=text.prof_false)
 
 # Создаем объект клавиатуры, добавляя в него кнопки
-keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_1, button_2]],
-                                                    resize_keyboard=True,
-                                                    input_field_placeholder=text.Press_button
-                                                    )
+keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_1, button_2]])
 
