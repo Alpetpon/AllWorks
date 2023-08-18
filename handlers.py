1b8aac22560f3edb931cf91f72ec141abbc54d2a
Alpetpon <ALpetpon51pro@gmail.com>
2023-08-18 17:04:03 +0300

    Начал делать заполнение резюме в ручную

--- handlers.py
+++ handlers.py
@@ -9,21 +9,19 @@ import kb
 
 router = Router()
 
+
 class Search(StatesGroup):
-    start = State()# Стартовая позиция
-    hub_choose_spec = State()# Выбор специальности
-    hub_create_resume = State()# Cоздание резюме
-    hub_choose_job = State()# Оценка вакансий
-    create_resume = State()#Создание резюме
+    start = State()  # Стартовая позиция
+    hub_choose_spec = State()  # Выбор специальности
+    hub_create_resume = State()  # Cоздание резюме
+    hub_choose_job = State()  # Оценка вакансий
+    create_resume = State()  # Создание резюме
+
 
 class resume(StatesGroup):
     name = State()
     surname = State()
     patronymic = State()
-
-
-
-
 
 
 # Обработчик комнды /start
@@ -32,6 +30,7 @@ async def process_start_handler(message: Message, state: FSMContext) -> None:
     await state.set_state(Search.start)
     await message.answer(text.greet.format(name=message.from_user.full_name), reply_markup=kb.keyboard_prof)
 
+
 # Обработчик команды /help
 @router.message(Command(commands=['help']))
 async def process_help_command(message: Message):
@@ -39,10 +38,11 @@ async def process_help_command(message: Message):
 
 
 # Обработичк нажатия кнопки для перехода к вакансиям
-@router.message(Text(text = text.prof_false))
+@router.message(Text(text=text.prof_false))
 async def process_prof_false_hendler(message: Message, state: FSMContext) -> None:
     await state.set_state(Search.hub_choose_spec)
-    await message.answer(text.job_text.format(name = message.from_user.full_name), reply_markup=kb.keyboard_job_1)
+    await message.answer(text.job_text.format(name=message.from_user.full_name), reply_markup=kb.keyboard_job_1)
+
 
 # Обработичк нажатия кнопки выбора специализации
 @router.message(Text(text=text.spis))
@@ -50,21 +50,18 @@ async def process_create_resume_hendler(message: Message, state: FSMContext) -> 
     await state.set_state(Search.hub_create_resume)
     await message.answer(text.resume, reply_markup=kb.keyboard_job_2)
 
+
 @router.message(Text(text=text.check_job))
 async def process_check_job(message: Message, state: FSMContext) -> None:
     await state.set_state(Search.hub_choose_job)
-    await message.answer(text.test,reply_markup=kb.keyboard_job_3)
+    await message.answer(text.test, reply_markup=kb.keyboard_job_3)
 
 
 @router.message(Text(text=text.create_resume))
 async def process_create_resume(message: Message, state: FSMContext) -> None:
     await state.set_state(Search.create_resume)
     await message.answer("Введите свое имя:")
-    await resume.set_state()
     await message.answer("Резюме успешно создано! Теперь выберите следующее действие.", reply_markup=kb.keyboard_job_3)
-
-
-
 
 
 # Обработчик кнопки "Назад"
