8c8880d14804ad4f46803aa1c00d5a31140dafb8
Alpetpon <ALpetpon51pro@gmail.com>
2023-08-18 16:30:07 +0300

    Изменения под handlers

--- kb.py
+++ kb.py
@@ -1,33 +1,17 @@
-from aiogram import types
 from aiogram.types import (KeyboardButton, ReplyKeyboardMarkup,ReplyKeyboardRemove)
 import text
 
-
-button_1: KeyboardButton = KeyboardButton(text=text.prof_true)#Кнопка для прохождения теста
-button_2: KeyboardButton = KeyboardButton(text=text.prof_false)#Кнопка для поиска работы без теста
+button_1: KeyboardButton = KeyboardButton(text=text.prof_false)#Кнопка для поиска работы без теста
 
 #Клавиатура №1
-keyboard_prof: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_1], [button_2]],  resize_keyboard=True)#Клавиатура которая встречает пользователя
+keyboard_prof: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_1]],  resize_keyboard=True)#Клавиатура которая встречает пользователя
 
 
-button_3: KeyboardButton = KeyboardButton(text=text.contin)#Кнопка Продолжить
-button_4: KeyboardButton = KeyboardButton(text=text.back)#Кнопка Назад, которая возварщает к клавиатуре №1
+button_2: KeyboardButton = KeyboardButton(text = text.spis)
+button_3: KeyboardButton = KeyboardButton(text = text.spis)
+button_4: KeyboardButton = KeyboardButton(text = text.search)
 
-#Клавиатура №2
-keyboard_prof_1: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_3], [button_4]],  resize_keyboard=True)
-
-button_5: KeyboardButton = KeyboardButton(text=text.go_resum)
-button_6: KeyboardButton = KeyboardButton(text=text.back)
-button_7: KeyboardButton = KeyboardButton(text=text.reset)
-
-keyboard_prof_2: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_5], [button_7], [button_6]],  resize_keyboard=True)
-
-button_8: KeyboardButton = KeyboardButton(text = text.spis)
-button_9: KeyboardButton = KeyboardButton(text = text.spis)
-button_10: KeyboardButton = KeyboardButton(text = text.search)
-button_11: KeyboardButton = KeyboardButton(text = text.back)
-
-keyboard_job_1: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_8], [button_9], [button_10], [button_11]],  resize_keyboard=True)
+keyboard_job_1: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_2], [button_3], [button_4]],  resize_keyboard=True)
 
 button_12: KeyboardButton = KeyboardButton(text = text.create_resume)
 button_13: KeyboardButton = KeyboardButton(text = text.create_resume_II)
