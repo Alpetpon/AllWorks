08d1cb4295f01e634af4ceda6b84c0d0a7240524
Alpetpon <ALpetpon51pro@gmail.com>
2023-08-19 17:03:14 +0300

    Новые клавиатуры

--- kb.py
+++ kb.py
@@ -1,31 +1,53 @@
-from aiogram.types import (KeyboardButton, ReplyKeyboardMarkup,ReplyKeyboardRemove)
+from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 import text
 
-button_1: KeyboardButton = KeyboardButton(text=text.prof_false)#Кнопка для поиска работы без теста
+start_search_button: KeyboardButton = KeyboardButton(text=text.start_search_job)  # Кнопка для поиска работы без теста
 
-#Клавиатура №1
-keyboard_prof: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_1]],  resize_keyboard=True)#Клавиатура которая встречает пользователя
+# Клавиатура №1
+start_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[start_search_button]],
+                                                          resize_keyboard=True)  # Клавиатура которая встречает пользователя
 
-
-button_2: KeyboardButton = KeyboardButton(text = text.spis)
-button_3: KeyboardButton = KeyboardButton(text = text.spis)
-button_4: KeyboardButton = KeyboardButton(text = text.search)
-
-keyboard_job_1: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_2], [button_3], [button_4]],  resize_keyboard=True)
-
-button_12: KeyboardButton = KeyboardButton(text = text.create_resume)
-button_13: KeyboardButton = KeyboardButton(text = text.create_resume_II)
-button_14: KeyboardButton = KeyboardButton(text = text.check_job)
-button_15: KeyboardButton = KeyboardButton(text = text.back)
+Create_resume: KeyboardButton = KeyboardButton(text=text.create_resume)
+Create_resume_II: KeyboardButton = KeyboardButton(text=text.create_resume_II)
+Check_job: KeyboardButton = KeyboardButton(text=text.check_job)
 
 
-keyboard_job_2: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_12, button_13], [button_15, button_14]], resize_keyboard=True)
+# Клавиатура №2
+Main_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[Create_resume], [Create_resume_II], [Check_job]],
+                                                          resize_keyboard=True) # Клавиатура главного меню
 
-button_16: KeyboardButton = KeyboardButton(text = text.like)
-button_17: KeyboardButton = KeyboardButton(text = text.dis)
-button_18: KeyboardButton = KeyboardButton(text = text.go_to_chat)
-button_19: KeyboardButton = KeyboardButton(text = text.about)
-button_20: KeyboardButton = KeyboardButton(text = text.back)
+like_button: KeyboardButton = KeyboardButton(text=text.like)
+dislike_button: KeyboardButton = KeyboardButton(text=text.dis)
+Chat_button: KeyboardButton = KeyboardButton(text=text.go_to_chat)
+About_button: KeyboardButton = KeyboardButton(text=text.about)
+Back_button: KeyboardButton = KeyboardButton(text=text.back)
+
+# Клавиатура №3
+live_check_job: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
+    keyboard=[[like_button, dislike_button], [Chat_button, About_button], [Back_button]], resize_keyboard=True)# Клавиатура для просмотра анкет
 
 
-keyboard_job_3: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[button_16, button_17], [button_18, button_19], [button_20]], resize_keyboard=True)
+admin_button: KeyboardButton = KeyboardButton(text = text.admin)
+# Клавиатура №4
+admin_panel: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[start_search_button], [admin_button]], resize_keyboard=True)
+
+
+Stat_button:KeyboardButton = KeyboardButton(text = text.stat)
+Block_button:KeyboardButton = KeyboardButton(text = text.block_text)
+Unlock_button:KeyboardButton = KeyboardButton(text = text.unlock_text)
+
+# Клавиатура №5
+Main_panel:ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard = [[Stat_button], [Block_button], [Unlock_button], [Back_button]])
+
+
+
+
+# Клавиатура пока не используется
+spec_button: KeyboardButton = KeyboardButton(text=text.spis)
+spec_button_1: KeyboardButton = KeyboardButton(text=text.spis)
+search_button: KeyboardButton = KeyboardButton(text=text.search)
+
+
+
+keyboard_job_1: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard=[[spec_button], [spec_button_1], [search_button]],
+                                                          resize_keyboard=True)# Клавиатура для выбора или поиска специальности
