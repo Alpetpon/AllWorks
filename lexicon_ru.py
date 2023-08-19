4efed4456012ea5a2c313368781f1d53467e777b
Alpetpon <ALpetpon51pro@gmail.com>
2023-08-19 17:03:34 +0300

    Функция просмотра id

--- lexicon_ru.py
+++ lexicon_ru.py
@@ -1,5 +1,5 @@
 LEXICON_COMMANDS_RU: dict[str, str] = {
                 '/start': 'Перезапуск бота',
                 '/help': 'Помощь с возникшеми проблемами',
-                '/command_3': 'command_3 desription',
+                '/id': 'Получение id пользователя',
                 '/command_4': 'command_4 desription'}
