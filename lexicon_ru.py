55a0433f5863209037ff777815f02fc90f810730
Alpetpon <ALpetpon51pro@gmail.com>
2023-08-21 15:57:55 +0300

    Добавлена функция /cancel

--- lexicon_ru.py
+++ lexicon_ru.py
@@ -2,4 +2,4 @@ LEXICON_COMMANDS_RU: dict[str, str] = {
                 '/start': 'Перезапуск бота',
                 '/help': 'Помощь с возникшеми проблемами',
                 '/id': 'Получение id пользователя',
-                '/command_4': 'command_4 desription'}
+                '/cancel': 'Команда для отмены'}
