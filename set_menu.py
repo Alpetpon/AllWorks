ea820770f4f5ea4138c79cbd279f9ad0f188d7a3
Alpetpon <ALpetpon51pro@gmail.com>
2023-08-25 14:14:20 +0300

    Change: Оставил комментарии

--- set_menu.py
+++ set_menu.py
@@ -2,12 +2,13 @@ from aiogram import Bot
 from aiogram.types import BotCommand
 from lexicon_ru import LEXICON_COMMANDS_RU
 
-
-# Функция для настройки кнопки Menu бота
+# Асинхронная функция для настройки главного меню бота
 async def set_main_menu(bot: Bot):
+    # Создаем список команд для главного меню бота
     main_menu_commands = [BotCommand(
-                                command=command,
-                                description=description
-                          ) for command,
-                                description in LEXICON_COMMANDS_RU.items()]
+        command=command,
+        description=description
+    ) for command, description in LEXICON_COMMANDS_RU.items()]
+
+    # Устанавливаем команды меню для бота
     await bot.set_my_commands(main_menu_commands)
