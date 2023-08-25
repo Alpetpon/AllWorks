406bd5e57b3cd83f6c72a18e8e170a310404d669
Alpetpon <ALpetpon51pro@gmail.com>
2023-08-25 14:13:55 +0300

    Change: Оставил комментарии

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-BOT_TOKEN = "6638436705:AAFsw-ScDPWuatwXf0baRh3yZZ2SSSMNSOg"
-admin_alex_id = 1926704070
+BOT_TOKEN = "6638436705:AAFsw-ScDPWuatwXf0baRh3yZZ2SSSMNSOg" # Токен для работы бота
+admin_alex_id = 1926704070 # Телеграмм id, который можно получить в боте прописав команду id
